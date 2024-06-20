e6316a971e7c5560f2a2b45cb9aff8be85f3e51d
Alfredo <alfloz01@ucm.es>
2024-06-20 14:59:47 +0200

    Create Task Card and get user id and delete task and correct vercel deploiment

--- src/app/api/users/[id]/route.ts
+++ src/app/api/users/[id]/route.ts
@@ -2,12 +2,18 @@ import db from "@/app/lib/prisma";
 
 export default async function GET(id: string) {
   try {
+    if (!id) {
+      throw new Error("ID parameter is required.");
+    }
+
     const user = await db.user.findUnique({
       where: { id: id },
     });
+
     if (!user) {
       return { message: "User not found" };
     }
+
     return user;
   } catch (error) {
     console.error("Error getting user:", error);
